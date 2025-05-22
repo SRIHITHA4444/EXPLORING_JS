80cf409cd61d602965cd541ed81a4c077c0e9508
SRIHITHA PAPAKARI <sriminnu2004@gmail.com>
2025-05-22 22:04:41 +0530

    1_scripts.js

--- 1_COLOR_CHANGER/1_script.js
+++ 1_COLOR_CHANGER/1_script.js
@@ -4,7 +4,11 @@ const body = document.querySelector('body');
 buttons.forEach( (buttonEle) => {
     //console.log(buttonEle);
     buttonEle.addEventListener('click', (et) => {
-        console.log(et);
-        console.log(et.target);        
-    })
-})
+        //console.log(et);
+        //console.log(et.target);
+
+        if(et.target.id === 'pink' || et.target.id === 'violet' ||et.target.id === 'magenta' || et.target.id === 'cadetblue'){
+            body.style.backgroundColor = et.target.id;
+        }
+    });
+});
