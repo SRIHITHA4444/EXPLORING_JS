dbbc7556bd171714ad0d0267b2977652d4efd5c5
SRIHITHA PAPAKARI <sriminnu2004@gmail.com>
2025-05-26 21:03:35 +0530

    8_script.js

--- 8_JOKES/8_script.js
+++ 8_JOKES/8_script.js
@@ -1,1 +1,16 @@
-const button = document.querySelector('button');
+const button = document.querySelector("button");
+
+const jokes = [
+    "Why was 6 afraid of 7? Because 7,8,9.",
+    "Why can't you ever trust atoms? Because they make up everything.",
+    "What kind of keys are sweet? Cookies!",
+    "What does a storm cloud wear under his raincoat? Thunderwear.",
+    "What do you call an ant who fights crime? A vigilANTe!",
+    "Why were the teacher’s eyes crossed? She couldn’t control her pupils.",
+    "How does the ocean say hello? It waves.",
+    "Why did the drum take a nap? It was beat",
+    "Why are teddy bears never hungry? Because they’re always stuffed!",
+    "What’s an astronaut’s favorite candy? A Mars bar.",
+    "Why did the tree go to the dentist? It needed a root canal.",
+    "Why is Peter Pan always flying? He Neverlands.",
+]
