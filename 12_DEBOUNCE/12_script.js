537518ced78043944bd05dfd9e78f08ba496defc
SRIHITHA PAPAKARI <sriminnu2004@gmail.com>
2025-05-29 22:08:09 +0530

    12_script.js

--- 12_DEBOUNCE/12_script.js
+++ 12_DEBOUNCE/12_script.js
@@ -1,53 +1,3 @@
-// function debounce(timeSet, delay){
-//     let timer;
-
-//     return function(){
-//         clearTimeout(timer);
-//         timer = setTimeout(() => timeSet, delay);
-//     }
-// }
-
-// const ip = document.querySelector('#user-input');
-
-// async function fetchData(url){
-//     try{
-//         const response = await fetch(url);
-//         return await response.json();
-//     }
-//     catch (e){
-//         throw `${e.message}`;
-//     }
-// }
-
-// const api = document.querySelector('.api-card')
-
-// ip.addEventListener('keydown', async (et) => {
-//     if(et.key === 'Enter'){
-//         et.preventDefault();
-
-//         const data = await fetchData(et.target.value);
-
-//         if(!data) return "";
-
-//         api.innerHTML = "";
-
-//         const img = document.createElement('img');   
-//         img.src = data.avatar_url || "";
-//         img.alt = "profile";
-//         api.appendChild(img);
-
-//         const h2 = document.createElement('h2');
-//         h2.innerText = data.name || "";
-//         api.appendChild(h2);
-
-//         const p = document.createElement('p');
-//         p.innerHTML = `PUBLIC REPOSITORIES: ${data.public_repos || 0}`;
-//         api.appendChild(p);
-
-//         api.style.display = 'block';
-//     }   
-// });
-
 const ip = document.querySelector('#user-input');
 const api = document.querySelector('.api-card');
 
@@ -56,7 +6,8 @@ function debounce(timeSet, delay){
 
     return function(...args){
         clearTimeout(timer);
-        timer = setTimeout(() => timeSet.apply(this, args), delay);
+        timer = setTimeout(() => timeSet(...args), delay); 
+        //can also use timeSet.apply(this, args)
     };
 }
 
